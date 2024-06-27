2a09890068a1623076caa1a2e6f80fcab9903794
phlyk <philippe.keighley@erm.com>
2024-06-27 22:01:06 +0100

    Created an ID Generator class to handle making Auto Incrementing PKs

--- app/reference_endpoints.py
+++ app/reference_endpoints.py
@@ -1,8 +1,22 @@
-from typing import List
+from typing import Dict, List, Type
 from fastapi import APIRouter, Query
 from pydantic import BaseModel
 
 router = APIRouter()
+
+
+class IDGenerator:
+    def __init__(self, models: list[Type[BaseModel]]):
+        self._counters: Dict[str, int] = {model.__name__: 0 for model in models}
+
+    def next_id(self, model: Type[BaseModel]) -> int:
+        model_name = model.__name__
+        if model_name not in self._counters:
+            raise ValueError(
+                f"Model {model_name} is not registered with this IDGenerator"
+            )
+        self._counters[model_name] += 1
+        return self._counters[model_name]
 
 
 class Item(BaseModel):
@@ -11,26 +25,26 @@ class Item(BaseModel):
     price: float
 
 
+id_generator = IDGenerator([Item])
 items_db: List[Item] = []
 
 
 @router.get("/")
-async def read_root():
+def read_root():
     return {"Hello": "World"}
 
 
 @router.get("/items/{item_id}")
-async def get_item(
+def get_item(
     item_id: int,
     query_string: str | None = Query(None, alias="item-query"),
 ):
-    for item in items_db:
-        if item.id == item_id:
-            return {"item_id": item.id, "q": query_string}
-    return {"item_id": "not found", "q": query_string}
+    return {"item_id": item_id, "q": query_string}
 
 
 @router.post("/items")
-async def create_item(item: Item):
-    items_db.append(item)
-    return item
+def create_item(item: Item):
+    new_id = id_generator.next_id(Item)
+    new_item = Item(new_id, **item.model_dump(exclude_unset=True))
+    items_db.append(new_item)
+    return new_item
