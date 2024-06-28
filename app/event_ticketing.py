190b7c8715e53d939d00dec1ddc61153112d7539
phlyk <philippe.keighley@erm.com>
2024-06-28 09:35:53 +0100

    Added the IDGenerator to the event_ticketing skeleton

--- app/event_ticketing.py
+++ app/event_ticketing.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 
+from app.reference_endpoints import IDGenerator
+
 router = APIRouter()
 
 
 class Event(BaseModel):
     pass
+
+
+id_generator = IDGenerator([Event])
