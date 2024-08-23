d96bc59b9e1d865e9be6a16456753a9588f8825b
phlyk <philippe.keighley@erm.com>
2024-08-23 13:19:35 +0100

    adjusted reference_endpoints for clarity for those unfamiliar with FastAPI

--- app/reference_endpoints.py
+++ app/reference_endpoints.py
@@ -14,15 +14,18 @@ class IDGenerator:
         """Generate the next ID for a Model. Must be a Model passed in at instantiation"""
         model_name = model.__name__
         if model_name not in self._counters:
-            raise ValueError(
-                f"Model {model_name} is not registered with this IDGenerator"
-            )
+            raise ValueError(f"Model {model_name} is not registered with this IDGenerator")
         self._counters[model_name] += 1
         return self._counters[model_name]
 
 
 class Item(BaseModel):
     id: int
+    name: str
+    price: float
+
+
+class CreateItem(BaseModel):
     name: str
     price: float
 
@@ -41,12 +44,13 @@ def get_item(
     item_id: int,
     query_string: str | None = Query(None, alias="item-query"),
 ):
+    # this method is just to show how to use Query with an alias
     return {"item_id": item_id, "q": query_string}
 
 
 @router.post("/items", include_in_schema=False)
-def create_item(item: Item):
+def create_item(item: CreateItem) -> Item:
     new_id = id_generator.next_id(Item)
-    new_item = Item(new_id, **item.model_dump(exclude={"id"}))
+    new_item = Item(id=new_id, **item.model_dump(exclude={"id"}))
     items_db.append(new_item)
     return new_item
